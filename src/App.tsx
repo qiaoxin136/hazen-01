426140370e37ebd3a073f0773e3e7d1cc254f991
Xin Qiao <qiaoxin136@gmail.com>
2025-02-18 08:18:02 -0500

    add map 1

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import "@aws-amplify/ui-react/styles.css";
 
 import {
   ScrollView,
-  //View,
+  View,
   Flex,
   Heading,
   Button,
@@ -29,10 +29,7 @@ import {
   Tabs,
 } from "@aws-amplify/ui-react";
 
-import {
-  Marker,
-
-} from "react-map-gl";
+import { Marker } from "react-map-gl";
 
 import axios, { AxiosResponse } from "axios";
 import type { Feature, Geometry } from "geojson";
@@ -82,7 +79,7 @@ const INITIAL_VIEW_STATE: any = {
   longitude: -96.20321,
   //latitude: 35.68111419325676,
   latitude: 37.00068,
-  zoom: 4.5,
+  zoom: 4,
   bearing: 0,
   pitch: 0,
 };
@@ -240,10 +237,14 @@ function App() {
 
   return (
     <main>
+      <Heading width="50vw" level={5}>
+        Hydraulic Modeling Group Sewer Software
+      </Heading>
+      <br />
+      
+      <br />
+      <br />
       <Flex>
-        <Heading width="50vw" level={5}>
-          Hydraulic Modeling Group Sewer Software
-        </Heading>
         <Button onClick={signOut} width={120}>
           Sign out
         </Button>
@@ -261,198 +262,198 @@ function App() {
           Map
         </Button>
       </Flex>
-      <Divider orientation="horizontal" />
       <br />
-      <Flex direction={"row"}>
-        <Input
-          type="text"
-          value={name}
-          placeholder="Project"
-          onChange={handleName}
-          size="small"
-        />
-        <Input
-          type="text"
-          value={customer}
-          placeholder="Customer"
-          onChange={handleCustomer}
-          size="small"
-        />
-        <Input
-          type="text"
-          value={location}
-          placeholder="Location"
-          onChange={handleLocation}
-          size="small"
-        />
-        <Input
-          type="date"
-          value={yearcompl}
-          placeholder="Completion"
-          onChange={handleYearcompl}
-          size="small"
-        />
-        <SelectField
-          label=""
-          placeholder=""
-          value={software}
-          onChange={(e) => setSoftware(e.target.value)}
-          width="150%"
-        >
-          <option value="infoworkscs">InfoWorks CS</option>
-          <option value="infoworksicm">InfoWorks ICM</option>
-          <option value="pcswmm">PCSWMM</option>
-          <option value="mike">MIKE(Urban)</option>
-          <option value="infoswmm">InfoSWMM</option>
-          <option value="infosewer">InfoSewer</option>
-          <option value="xpswmm">XPSWMM</option>
-          <option value="sewergems">SewerGEMS</option>
-          <option value="watergems">WaterGEMS</option>
-          <option value="h2osewer">H2OMap Sewer</option>
-          <option value="infowater">InfoWater</option>
-          <option value="aquatwin">AquaTwin</option>
-        </SelectField>
-        <Input
-          type="number"
-          value={mile}
-          placeholder="Mile"
-          onChange={handleMile}
-          size="small"
-          width="50%"
-        />
-        <Input
-          type="number"
-          value={ps}
-          placeholder="PS"
-          onChange={handlePs}
-          size="small"
-          width="50%"
-        />
-        <Input type="number" value={lat} onChange={handleLat} />
-        <Input type="number" value={lng} onChange={handleLng} />
-      </Flex>
-      <Tabs
-        value={tab}
-        onValueChange={(tab) => setTab(tab)}
-        items={[
-          {
-            label: "Project Table",
-            value: "1",
-            content: (
-              <>
-                <ScrollView
-                  as="div"
-                  ariaLabel="View example"
-                  backgroundColor="var(--amplify-colors-white)"
-                  borderRadius="6px"
-                  //border="1px solid var(--amplify-colors-black)"
-                  // boxShadow="3px 3px 5px 6px var(--amplify-colors-neutral-60)"
-                  color="var(--amplify-colors-blue-60)"
-                  // height="45rem"
-                  // maxWidth="100%"
-                  padding="1rem"
-                  // width="100%"
-                  width="2400px"
-                  height={"2400px"}
-                  maxHeight={"2400px"}
-                  maxWidth="2400px"
-                >
-                  <ThemeProvider theme={theme} colorMode="light">
-                    <Table
-                      caption=""
-                      highlightOnHover={true}
-                      variation="striped"
-                    >
-                      <TableHead>
-                        <TableRow>
-                          <TableCell as="th">Project</TableCell>
-                          <TableCell as="th">Customer</TableCell>
-                          <TableCell as="th">Location</TableCell>
-                          <TableCell as="th">Completion</TableCell>
-                          <TableCell as="th">Software</TableCell>
-                          <TableCell as="th">Miles</TableCell>
-                          <TableCell as="th">PS</TableCell>
-                          <TableCell as="th">Latitude</TableCell>
-                          <TableCell as="th">Longitude</TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {todos.map((todo) => (
-                          <TableRow
-                            onClick={() => deleteTodo(todo.id)}
-                            key={todo.yearcompl}
-                          >
-                            <TableCell>{todo.name}</TableCell>
-                            <TableCell>{todo.customer}</TableCell>
-                            <TableCell>{todo.location}</TableCell>
-                            <TableCell>{todo.yearcompl}</TableCell>
-                            <TableCell>{todo.software}</TableCell>
-                            <TableCell>{todo.mile}</TableCell>
-                            <TableCell>{todo.ps}</TableCell>
-                            <TableCell>{todo.lat}</TableCell>
-                            <TableCell>{todo.lng}</TableCell>
-                          </TableRow>
-                        ))}
-                      </TableBody>
-                    </Table>
-                  </ThemeProvider>
-                </ScrollView>
-              </>
-            ),
-          },
-          {
-            label: "Project Map",
-            value: "2",
-            content: (
-              <>
-                <ScrollView>
-                  <DeckGL
-                    initialViewState={INITIAL_VIEW_STATE}
-                    controller
-                    layers={layers}
-                    onClick={onClick}
-                    onViewStateChange={({ viewState }) =>
-                      setViewport(viewState)
-                    }
-                    style={{
-                      // height: "100%",
-                      // width: "100%",
-                      top: "15%",
-                    }}
+    
+
+      <Flex direction={"column"}>
+        <Flex direction={"row"} width={"100%"}>
+          <Input
+            type="text"
+            value={name}
+            placeholder="Project"
+            onChange={handleName}
+            size="small"
+             width="50%"
+          />
+          <Input
+            type="text"
+            value={customer}
+            placeholder="Customer"
+            onChange={handleCustomer}
+            size="small"
+             width="50%"
+          />
+          <Input
+            type="text"
+            value={location}
+            placeholder="Location"
+            onChange={handleLocation}
+            size="small"
+             width="50%"
+          />
+          <Input
+            type="date"
+            value={yearcompl}
+            placeholder="Completion"
+            onChange={handleYearcompl}
+            size="small"
+             width="50%"
+          />
+          <SelectField
+            label=""
+            placeholder=""
+            value={software}
+            onChange={(e) => setSoftware(e.target.value)}
+            width="80%"
+          >
+            <option value="infoworkscs">InfoWorks CS</option>
+            <option value="infoworksicm">InfoWorks ICM</option>
+            <option value="pcswmm">PCSWMM</option>
+            <option value="mike">MIKE(Urban)</option>
+            <option value="infoswmm">InfoSWMM</option>
+            <option value="infosewer">InfoSewer</option>
+            <option value="xpswmm">XPSWMM</option>
+            <option value="sewergems">SewerGEMS</option>
+            <option value="watergems">WaterGEMS</option>
+            <option value="h2osewer">H2OMap Sewer</option>
+            <option value="infowater">InfoWater</option>
+            <option value="aquatwin">AquaTwin</option>
+          </SelectField>
+          <Input
+            type="number"
+            value={mile}
+            placeholder="Mile"
+            onChange={handleMile}
+            size="small"
+            width="50%"
+          />
+          <Input
+            type="number"
+            value={ps}
+            placeholder="PS"
+            onChange={handlePs}
+            //size="small"
+            width="50%"
+          />
+          <Input type="number" value={lat} onChange={handleLat} />
+          <Input type="number" value={lng} onChange={handleLng} />
+        </Flex>
+        <Divider orientation="horizontal" />
+        <Tabs
+          value={tab}
+          onValueChange={(tab) => setTab(tab)}
+          items={[
+            {
+              label: "Project Table",
+              value: "1",
+              content: (
+                <>
+                  <ScrollView
+                    as="div"
+                    ariaLabel="View example"
+                    backgroundColor="var(--amplify-colors-white)"
+                    borderRadius="6px"
+                    border="1px solid var(--amplify-colors-black)"
+                     boxShadow="3px 3px 5px 6px var(--amplify-colors-neutral-60)"
+                    color="var(--amplify-colors-blue-60)"
+                    // height="45rem"
+                    // maxWidth="100%"
+                    padding="1rem"
+                    // width="100%"
+                    // width="2400px"
+                    // height={"2400px"}
+                    // maxHeight={"2400px"}
+                    // maxWidth="2400px"
                   >
-                    <MapView
-                      {...viewport}
+                    <ThemeProvider theme={theme} colorMode="light">
+                      <Table
+                        caption=""
+                        highlightOnHover={false}
+                        variation="striped"
+                      >
+                        <TableHead>
+                          <TableRow>
+                            <TableCell as="th">Project</TableCell>
+                            <TableCell as="th">Customer</TableCell>
+                            <TableCell as="th">Location</TableCell>
+                            <TableCell as="th">Completion</TableCell>
+                            <TableCell as="th">Software</TableCell>
+                            <TableCell as="th">Miles</TableCell>
+                            <TableCell as="th">PS</TableCell>
+                            <TableCell as="th">Latitude</TableCell>
+                            <TableCell as="th">Longitude</TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {todos.map((todo) => (
+                            <TableRow
+                              onClick={() => deleteTodo(todo.id)}
+                              key={todo.yearcompl}
+                            >
+                              <TableCell>{todo.name}</TableCell>
+                              <TableCell>{todo.customer}</TableCell>
+                              <TableCell>{todo.location}</TableCell>
+                              <TableCell>{todo.yearcompl}</TableCell>
+                              <TableCell>{todo.software}</TableCell>
+                              <TableCell>{todo.mile}</TableCell>
+                              <TableCell>{todo.ps}</TableCell>
+                              <TableCell>{todo.lat}</TableCell>
+                              <TableCell>{todo.lng}</TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </ThemeProvider>
+                  </ScrollView>
+                </>
+              ),
+            },
+            {
+              label: "Project Map",
+              value: "2",
+              content: (
+                <>
+                  <View
+                    // width="100%"
+                    //  width="100%"
+                    //  height={"1600px"}
+                    //  maxHeight={"1600px"}
+                    //  maxWidth="1600px"
+                  >
+                    <DeckGL
                       initialViewState={INITIAL_VIEW_STATE}
+                      controller
+                      layers={layers}
+                      onClick={onClick}
+                      onViewStateChange={({ viewState }) =>
+                        setViewport(viewState)
+                      }
                       style={{
-                        //position: "absolute",
-                        zIndex: -1,
-                        // height: "100%",
-                        // width: "100%",
+                        height: "100%",
+                        width: "1600px",
+                        top: "30%",
                       }}
                     >
-                       
-                      <Marker latitude={lat} longitude={lng}></Marker>
-                    </MapView>
-                  </DeckGL>
-                </ScrollView>
-              </>
-            ),
-          },
-        ]}
-      />
-      {/* <View
-        as="div"
-        ariaLabel="View example"
-        backgroundColor="var(--amplify-colors-white)"
-        borderRadius="6px"
-        //border="1px solid var(--amplify-colors-black)"
-        // boxShadow="3px 3px 5px 6px var(--amplify-colors-neutral-60)"
-        color="var(--amplify-colors-blue-60)"
-        height="45rem"
-        // maxWidth="100%"
-        padding="1rem"
-        width="100%"
-      ></View> */}
+                      <MapView
+                        {...viewport}
+                        initialViewState={INITIAL_VIEW_STATE}
+                        style={{
+                          position: "absolute",
+                          zIndex: -1,
+                          height: "100%",
+                          width: "1600px",
+                        }}
+                      >
+                        <Marker latitude={lat} longitude={lng}></Marker>
+                      </MapView>
+                    </DeckGL>
+                  </View>
+                </>
+              ),
+            },
+          ]}
+        />
+      </Flex>
     </main>
   );
 }
